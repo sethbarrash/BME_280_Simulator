f7fafd22e07884c686922ac9e709fee60d41dfa6
sethbarrash <sethbarrash@gmail.com>
2024-05-03 21:22:17 -0500

    Finish writing test_read_chip_id

--- test/test_BME_280_Simulator.cpp
+++ test/test_BME_280_Simulator.cpp
@@ -2,7 +2,8 @@
 #include "BME_280_Simulator.h"
 #include "BME_280_random_memory_map.h"
 
-int main() {
+void test_unselected()
+{
   memory_map m = initialize_random_memory_map();
   BME_280_Simulator s(m);
   bool b;
@@ -13,15 +14,25 @@ int main() {
   b = s.process_cycle(1, 1);
   assert(b == DEFAULT_SDO);
   assert(s.state == STATE_WAITING);
+}
 
-  // Test read
+void test_read_chip_id() 
+{
+  memory_map m = initialize_random_memory_map();
+  BME_280_Simulator s(m);
+  bool b;
+
+  // Select Chip
   b = s.process_cycle(0, 0);
   assert(b == DEFAULT_SDO);
   assert(s.state == STATE_INSTRUCTION);
+
+  // Send read instruction
   b = s.process_cycle(1, 0);
   assert(b == DEFAULT_SDO);
   assert(s.state == STATE_ADDRESS);
 
+  // Send Chip ID register address
   assert(s.address_bit == 6);
   b = s.process_cycle(1, 0);
   assert(b == DEFAULT_SDO);
@@ -58,7 +69,51 @@ int main() {
   assert(b == DEFAULT_SDO);
   assert(s.memory_ptr == 0xD0);
 
+  // Read out chip ID
+  b = s.process_cycle(0, 0);
+  assert(s.state == STATE_TRANSMITTING);
+  assert(b == 0);
+
+  b = s.process_cycle(0, 0);
+  assert(s.state == STATE_TRANSMITTING);
+  assert(b == 1);
+
+  b = s.process_cycle(0, 0);
+  assert(s.state == STATE_TRANSMITTING);
+  assert(b == 1);
+
+  b = s.process_cycle(0, 0);
+  assert(s.state == STATE_TRANSMITTING);
+  assert(b == 0);
+
+  b = s.process_cycle(0, 0);
+  assert(s.state == STATE_TRANSMITTING);
+  assert(b == 0);
+
+  b = s.process_cycle(0, 0);
+  assert(s.state == STATE_TRANSMITTING);
+  assert(b == 0);
+
+  b = s.process_cycle(0, 0);
+  assert(s.state == STATE_TRANSMITTING);
+  assert(b == 0);
+
+  b = s.process_cycle(0, 0);
+  assert(s.state == STATE_WAITING);
+  assert(b == 0);
+}
+
+int main() {
+
+  test_unselected();
+
+  test_read_chip_id();
+
   // Test read invalid address
+
+  // Test interrupted read
+
+  // Test burst read
 
   // Test write
 
